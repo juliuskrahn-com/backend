d20c7b4e491120d3205acb2d1e834ed3d70a10ed
Julius Krahn <juliuskrahn@outlook.de>
2021-03-03 20:51:53 +0100

    Create cutom domain name for api, change region to us-east-1 -> region change -> couldn't get certificate (has to be in us-east-1 for EDGE deployment) to work with api-gatewy/... in eu-central-1

--- stack.py
+++ stack.py
@@ -3,6 +3,9 @@ import aws_cdk.aws_apigateway as apigw
 import aws_cdk.aws_lambda as lambda_
 import aws_cdk.aws_dynamodb as dynamodb
 import aws_cdk.aws_secretsmanager as sm
+import aws_cdk.aws_certificatemanager as cm
+import aws_cdk.aws_route53 as route53
+import aws_cdk.aws_route53_targets as route53_targets
 
 
 class Stack(core.Stack):
@@ -30,6 +33,18 @@ class Stack(core.Stack):
 
         proxy_fn_integration = apigw.LambdaIntegration(proxy_fn)
 
+        domain_name = apigw.DomainName(
+            self,
+            "blog-api-domain-name",
+            domain_name="api.juliuskrahn.com",
+            certificate=cm.Certificate.from_certificate_arn(
+                self,
+                "blog-domain-name-certificate",
+                "arn:aws:acm:us-east-1:473883619336:certificate/1ad12871-4b46-44ef-a24d-7af5ac43972b"
+            ),
+            endpoint_type=apigw.EndpointType.EDGE
+        )
+
         api = apigw.RestApi(
             self,
             "blog-api",
@@ -38,6 +53,16 @@ class Stack(core.Stack):
                 allow_methods=apigw.Cors.ALL_METHODS
             ),
             default_integration=proxy_fn_integration
+        )
+
+        domain_name.add_base_path_mapping(api)
+
+        route53.ARecord(
+            self,
+            "blog-api-a-record",
+            record_name="api",
+            target=route53.RecordTarget.from_alias(route53_targets.ApiGatewayDomain(domain_name)),
+            zone=route53.HostedZone.from_lookup(self, "blog-hosted-zone", domain_name="juliuskrahn.com")
         )
 
         api.root.add_proxy()

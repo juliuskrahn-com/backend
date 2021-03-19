aaf601727e8814583be8216e4b2b3438030f8cf8
Julius Krahn <juliuskrahn@outlook.de>
2021-03-19 06:33:06 +0100

    Fix article_get_collection lambda fn -> use paginate util properly

--- backend/lambda_functions/article_get_collection/lambda_function.py
+++ backend/lambda_functions/article_get_collection/lambda_function.py
@@ -6,7 +6,6 @@ article_table = middleware.get_article_table()
 
 @middleware.middleware
 def handler(event: middleware.Event, context):
-    articles = article_table.scan_paginate_items(ProjectionExpression="urlTitle,title,description,tag,published")\
-        .get("Items", [])
+    articles = article_table.scan_paginate_items(ProjectionExpression="urlTitle,title,description,tag,published")
     articles.sort(key=lambda item: item["published"], reverse=True)
     return middleware.Response(body={"articles": articles})

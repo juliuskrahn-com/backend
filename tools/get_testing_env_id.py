6d7b7ddc7fd57143a28ea54b0c62e8648aa8b5b1
Julius Krahn <juliuskrahn@outlook.de>
2021-03-23 08:28:14 +0100

    Set default testing env id to empty str
    Testing stack is synthed too on deploy and needs the id.

--- tools/get_testing_env_id.py
+++ tools/get_testing_env_id.py
@@ -1,13 +1,20 @@
 import sys
 import json
+import logging
 
 
 def get_testing_env_id():
-    with open("build/deploy_testing_context.json", "r") as f:
-        context = json.load(f)
-        env_id = context["EnvironmentId"]
-    if not env_id or type(env_id) != str:
-        raise ValueError
+    fp = "build/deploy_testing_context.json"
+    try:
+        with open(fp, "r") as f:
+            context = json.load(f)
+            env_id = context["EnvironmentId"]
+        if not env_id or type(env_id) != str:
+            raise ValueError(f"Testing environment id set in file: '{fp}' is invalid.")
+    except FileNotFoundError:
+        logging.warning(f"Testing environment id is now set to an empty string, "
+                        f"because file: '{fp}' wasn't found.")
+        env_id = ""
     return env_id
 
 
